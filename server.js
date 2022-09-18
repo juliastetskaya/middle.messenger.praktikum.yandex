ee9e23a0486ed3b0c1aa0fbb170bc48ed367bae8
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2022-09-18 18:49:12 +0300

    feat: Add express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
