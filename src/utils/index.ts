8f80b05bffddacbec6dd33062d87fe0ac4b9e835
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2022-11-09 16:41:29 +0300

    feat: Add HTTPTransport class

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { validateAndGetInputData } from './validateAndGetInputData';
 export { validateForm, ValidateType } from './validateForm';
+export { queryStringify } from './queryStringify';
