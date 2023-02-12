4d711d3050f59db2cadb2f7eb31d6987925d1d63
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2023-02-12 14:09:32 +0300

    feat: Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+        core: path.resolve(__dirname, "./src/core"),
+        utils: path.resolve(__dirname, "./src/utils"),
+        components: path.resolve(__dirname, "./src/components"),
+        static: path.resolve(__dirname, "./src/static"),
+        data: path.resolve(__dirname, "./src/data"),
+        services: path.resolve(__dirname, "./src/services"),
+        blocks: path.resolve(__dirname, "./src/blocks"),
+        HOC: path.resolve(__dirname, "./src/HOC"),
+        API: path.resolve(__dirname, "./src/API"),
+        handlebars: "handlebars/dist/handlebars.min.js",
+      }
+  },
+  devServer: {
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.(?:png|jpg|jpeg|woff(2)?|eot|ttf|otf|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+        filename: '[name].css'
+    })
+  ],
+};
